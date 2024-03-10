261aedf0095d21626708b574d8ebbf085240d57b
Bogdan <bohdan.m.baiurchak@ukd.edu.ua>
2024-03-10 16:20:35 +0200

    Add and style content information

--- src/components/StartPage/StartPage.tsx
+++ src/components/StartPage/StartPage.tsx
@@ -1,11 +1,11 @@
 import { Layout, Button, Upload, Typography, Flex, Row, Col } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import { UploadOutlined, DownloadOutlined,DashboardOutlined } from '@ant-design/icons';
 import './styleStartPage.scss';
 import logo from '../../assets/Designer.png';
 import { Container } from 'react-bootstrap';
 
 const { Header, Content, Footer } = Layout;
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 
 const StartPage = () => {
 
@@ -15,35 +15,49 @@ const StartPage = () => {
     };
     return (
         <Flex gap="middle" wrap="wrap">
-          <Layout className="layout">
-            <Header className="header">
-              <Button className="logo__container">
-                <img className="logo" src={logo} alt="logo" />
-                <Title className="logo__title" level={3}>
-                  Simple Editor
-                </Title>
-              </Button>
-            </Header>
-            <Content className="content">
-              <Container>
-                <Row>
-                  <Col span="12">
-                    <Title className='content__title' level={1}>Simple SVG editor</Title>
-                    
-                  </Col>
-                  <Col span="12">
-                    <Upload accept=".svg" beforeUpload={handleUpload} showUploadList={false}>
-                      <Button icon={<UploadOutlined />}>Завантажити SVG</Button>
-                    </Upload>
-                  </Col>
-                </Row>
-              </Container>
-            </Content>
-            <Footer></Footer>
-          </Layout>
+            <Layout className="layout">
+                <Header className="header">
+                    <Button className="logo__container">
+                        <img className="logo" src={logo} alt="logo" />
+                        <Title className="logo__title" level={3}>
+                            Simple Editor
+                        </Title>
+                    </Button>
+                </Header>
+                <Content className="content">
+                    <Container>
+                        <Row>
+                            <Col span="12">
+                                <Title className='content__title' level={1}>Simple SVG editor</Title>
+                                <ul className='content__list'>
+                                    <li>
+                                        <DownloadOutlined />
+                                        <Paragraph><strong>Step 1:</strong> Select the SVG image you want to edit and submit it here to the uploader at the right.</Paragraph>
+                                    </li>
+                                    <li>
+                                    <DashboardOutlined />
+                                        <Paragraph><strong>Step 2:</strong> Wait a moment; the editor will load in a few seconds and display your image.</Paragraph>
+                                    </li>
+                                    <li>
+                                        <DownloadOutlined />
+                                        <Paragraph><strong>Step 3:</strong> Start editing and save your image once done.</Paragraph>
+                                    </li>
+                                </ul>
+
+                            </Col>
+                            <Col span="12">
+                                <Upload accept=".svg" beforeUpload={handleUpload} showUploadList={false}>
+                                    <Button icon={<UploadOutlined />}>Завантажити SVG</Button>
+                                </Upload>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Content>
+                <Footer></Footer>
+            </Layout>
         </Flex>
-      );
-      
+    );
+
 };
 
 export default StartPage;

7f9e6b6d950e9577e70e489ffcdbbddfc491588c
staff <masterb21052017@gmail.com>
2024-03-13 16:48:56 +0200

    Break down action input by component

--- src/components/Workspace/SideComponent/SideComponent.tsx
+++ src/components/Workspace/SideComponent/SideComponent.tsx
@@ -1,56 +1,33 @@
 import Sider from "antd/es/layout/Sider";
 import styles from './style.module.scss';
-import { Col, ColorPicker, Divider, InputNumber, Row, Typography } from "antd";
+import { Col, Divider, Row, Typography } from "antd";
+import ActionInput from "./ActionInputComponents/InputAction";
+import ColorChange from "./ActionInputComponents/ColorChangeAction";
 
 
 const SideComponent = () => {
-
-    const onChangeWeight = (value: number) => {
-        console.log('changed', value);
-    };
-    const onChangeHeight = (value: number) => {
-        console.log('changed', value);
-    };
 
     return (
         <Sider width="20%" className={styles.aside}>
             <div className={styles.action__container}>
                 <Row>
                     <Col xs={12}>
-                        <div className={styles.input__container}>
-                            <span>X</span>
-                            <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
-                        </div>
+                        <ActionInput title="X" />
                     </Col>
                     <Col xs={12}>
-                        <div className={styles.input__container}>
-                            <span>Y</span>
-                            <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
-                        </div>
+                        <ActionInput title="Y" />
                     </Col>
                     <Col xs={12}>
-                        <div className={styles.input__container}>
-                            <span>Width</span>
-                            <InputNumber controls={false} min={1} defaultValue={1} onChange={() => onChangeWeight} />
-                        </div>
+                        <ActionInput title="Width" />
                     </Col>
                     <Col xs={12}>
-                        <div className={styles.input__container}>
-                            <span>Height</span>
-                            <InputNumber controls={false} min={1} defaultValue={1} onChange={() => onChangeHeight} />
-                        </div>
+                        <ActionInput title="Height" />
                     </Col>
                     <Col xs={12}>
-                        <div className={styles.input__container}>
-                            <span>Radius</span>
-                            <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
-                        </div>
+                        <ActionInput title="Radius" />
                     </Col>
                     <Col xs={12}>
-                        <div className={styles.input__container}>
-                            <span>Rotade</span>
-                            <InputNumber controls={false} min={-360} defaultValue={0} max={360} onChange={() => onChangeHeight} />
-                        </div>
+                        <ActionInput title="Rotate" />
                     </Col>
                 </Row>
             </div>
@@ -62,16 +39,10 @@ const SideComponent = () => {
                 </Row>
                 <Row>
                     <Col xs={12}>
-                        <div className={styles.input__container}>
-                            <span>Color</span>
-                            <ColorPicker defaultValue="#1677ff" />
-                        </div>
+                        <ColorChange title="Color"/>
                     </Col>
                     <Col xs={12}>
-                        <div className={styles.input__container}>
-                            <span>Border color</span>
-                            <ColorPicker defaultValue="#1677ff" />
-                        </div>
+                        <ColorChange title="Border color"/>
                     </Col>
                 </Row>
             </div>
@@ -82,10 +53,7 @@ const SideComponent = () => {
                 </Row>
                 <Row>
                     <Col xs={12}>
-                        <div className={styles.input__container}>
-                            <span>Border width</span>
-                            <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
-                        </div>
+                        <ActionInput title="Border width"/>
                     </Col>
                 </Row>
             </div>

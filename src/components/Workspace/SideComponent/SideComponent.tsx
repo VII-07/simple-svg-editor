be5d485de1431fb888b6abf50af5ccbe255be825
staff <masterb21052017@gmail.com>
2024-03-13 16:04:56 +0200

    Split sidebar on sections, add coords

--- src/components/Workspace/SideComponent/SideComponent.tsx
+++ src/components/Workspace/SideComponent/SideComponent.tsx
@@ -1,6 +1,6 @@
 import Sider from "antd/es/layout/Sider";
 import styles from './style.module.scss';
-import { Col, ColorPicker, InputNumber, Row } from "antd";
+import { Col, ColorPicker, Divider, InputNumber, Row, Typography } from "antd";
 
 
 const SideComponent = () => {
@@ -13,45 +13,82 @@ const SideComponent = () => {
     };
 
     return (
-        <Sider width="17%" className={styles.aside}>
-            <Row>
-                <Col xs={12}>
-                    <div className={styles.input__container}>
-                        <span>Wight</span>
-                        <InputNumber controls={false} min={1} defaultValue={1} onChange={() => onChangeWeight} />
-                    </div>
-                </Col>
-                <Col xs={12}>
-                    <div className={styles.input__container}>
-                        <span>Height</span>
-                        <InputNumber controls={false} min={1} defaultValue={1} onChange={() => onChangeHeight} />
-                    </div>
-                </Col>
-                <Col xs={12}>
-                    <div className={styles.input__container}>
-                        <span>Radius</span>
-                        <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
-                    </div>
-                </Col>
-                <Col xs={12}>
-                    <div className={styles.input__container}>
-                        <span>Rotade</span>
-                        <InputNumber controls={false} min={-360} defaultValue={0} max={360} onChange={() => onChangeHeight} />
-                    </div>
-                </Col>
-                <Col xs={12}>
-                    <div className={styles.input__container}>
-                        <span>Color</span>
-                        <ColorPicker  defaultValue="#1677ff" />
-                    </div>
-                </Col>
-                <Col xs={12}>
-                    <div className={styles.input__container}>
-                        <span>Border</span>
-                        <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
-                    </div>
-                </Col>
-            </Row>
+        <Sider width="20%" className={styles.aside}>
+            <div className={styles.action__container}>
+                <Row>
+                    <Col xs={12}>
+                        <div className={styles.input__container}>
+                            <span>X</span>
+                            <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
+                        </div>
+                    </Col>
+                    <Col xs={12}>
+                        <div className={styles.input__container}>
+                            <span>Y</span>
+                            <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
+                        </div>
+                    </Col>
+                    <Col xs={12}>
+                        <div className={styles.input__container}>
+                            <span>Wight</span>
+                            <InputNumber controls={false} min={1} defaultValue={1} onChange={() => onChangeWeight} />
+                        </div>
+                    </Col>
+                    <Col xs={12}>
+                        <div className={styles.input__container}>
+                            <span>Height</span>
+                            <InputNumber controls={false} min={1} defaultValue={1} onChange={() => onChangeHeight} />
+                        </div>
+                    </Col>
+                    <Col xs={12}>
+                        <div className={styles.input__container}>
+                            <span>Radius</span>
+                            <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
+                        </div>
+                    </Col>
+                    <Col xs={12}>
+                        <div className={styles.input__container}>
+                            <span>Rotade</span>
+                            <InputNumber controls={false} min={-360} defaultValue={0} max={360} onChange={() => onChangeHeight} />
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+
+            <Divider />
+            <div className={styles.action__container}>
+                <Row>
+                    <Col xs={12}><Typography.Title className={styles.action__title} level={4}>Fill</Typography.Title></Col>
+                </Row>
+                <Row>
+                    <Col xs={12}>
+                        <div className={styles.input__container}>
+                            <span>Color</span>
+                            <ColorPicker defaultValue="#1677ff" />
+                        </div>
+                    </Col>
+                    <Col xs={12}>
+                        <div className={styles.input__container}>
+                            <span>Border color</span>
+                            <ColorPicker defaultValue="#1677ff" />
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+            <Divider />
+            <div className={styles.action__container}>
+                <Row>
+                    <Col xs={12}><Typography.Title className={styles.action__title} level={4}>Border</Typography.Title></Col>
+                </Row>
+                <Row>
+                    <Col xs={12}>
+                        <div className={styles.input__container}>
+                            <span>Border wight</span>
+                            <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
+                        </div>
+                    </Col>
+                </Row>
+            </div>
         </Sider>
     );
 }

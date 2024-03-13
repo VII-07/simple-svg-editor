9b154b141ac623416932d50500c9f6732f2ffcc0
staff <masterb21052017@gmail.com>
2024-03-13 10:14:29 +0200

    change bg aside

--- src/components/Workspace/SideComponent/SideComponent.tsx
+++ src/components/Workspace/SideComponent/SideComponent.tsx
@@ -18,37 +18,37 @@ const SideComponent = () => {
                 <Col xs={12}>
                     <div className={styles.input__container}>
                         <span>Wight</span>
-                        <InputNumber min={1} defaultValue={1} onChange={() => onChangeWeight} />
+                        <InputNumber controls={false} min={1} defaultValue={1} onChange={() => onChangeWeight} />
                     </div>
                 </Col>
                 <Col xs={12}>
                     <div className={styles.input__container}>
                         <span>Height</span>
-                        <InputNumber min={1} defaultValue={1} onChange={() => onChangeHeight} />
+                        <InputNumber controls={false} min={1} defaultValue={1} onChange={() => onChangeHeight} />
                     </div>
                 </Col>
                 <Col xs={12}>
                     <div className={styles.input__container}>
                         <span>Radius</span>
-                        <InputNumber min={0} defaultValue={0} onChange={() => onChangeHeight} />
+                        <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
                     </div>
                 </Col>
                 <Col xs={12}>
                     <div className={styles.input__container}>
                         <span>Rotade</span>
-                        <InputNumber min={-360} defaultValue={0} max={360} onChange={() => onChangeHeight} />
+                        <InputNumber controls={false} min={-360} defaultValue={0} max={360} onChange={() => onChangeHeight} />
                     </div>
                 </Col>
                 <Col xs={12}>
                     <div className={styles.input__container}>
                         <span>Color</span>
-                        <ColorPicker defaultValue="#1677ff" />
+                        <ColorPicker  defaultValue="#1677ff" />
                     </div>
                 </Col>
                 <Col xs={12}>
                     <div className={styles.input__container}>
                         <span>Border</span>
-                        <InputNumber min={0} defaultValue={0} onChange={() => onChangeHeight} />
+                        <InputNumber controls={false} min={0} defaultValue={0} onChange={() => onChangeHeight} />
                     </div>
                 </Col>
             </Row>
